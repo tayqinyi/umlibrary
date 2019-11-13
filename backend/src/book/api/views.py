1c8df4d0306aeafbb991d25b55adedc85bfb2898
tayqinyi <37528962+tayqinyi@users.noreply.github.com>
2019-11-13 19:33:17 +0800

    finished backend bone

---
+++ backend/src/book/api/views.py
@@ -0,0 +1,11 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from book.models import Book
+from .serializers import BookSerializer
+
+class BookListView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetailView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
